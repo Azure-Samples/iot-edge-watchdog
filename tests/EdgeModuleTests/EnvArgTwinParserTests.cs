66aae5a307db69973c582f8863a7eceda8cce0b8
William Berry <wberry@microsoft.com>
2020-01-17 13:05:07 -0800

    update module unit test project to properly run tests

--- tests/EdgeModuleTests/EnvArgTwinParserTests.cs
+++ tests/EdgeModuleTests/EnvArgTwinParserTests.cs
@@ -16,14 +16,15 @@ namespace IoTHubListenerTests
         public void GetTimeSpanEnvVarTest()
         {
             var testEnvVar = "HB_FREQUENCY_IN_SECONDS";
-            TimeSpan defaultVal = TimeSpan.FromSeconds(10);
+            var testTimeSpan = 10;
+            TimeSpan defaultVal = TimeSpan.FromSeconds(testTimeSpan);
             var value = Environment.GetEnvironmentVariable(testEnvVar);
             // Create it if it's empty
             if (value == null) 
             {
-                Environment.SetEnvironmentVariable(testEnvVar, defaultVal.ToString());
+                Environment.SetEnvironmentVariable(testEnvVar, testTimeSpan.ToString());
                 
-                Assert.Equal(defaultVal, HeartbeatModule.Program.GetTimeSpanEnvVar(testEnvVar, defaultVal));
+                Assert.Equal(defaultVal, HeartbeatModule.Program.GetTimeSpanEnvVar(testEnvVar, testTimeSpan));
             }
             
             DeleteTestEnvVar(testEnvVar);
